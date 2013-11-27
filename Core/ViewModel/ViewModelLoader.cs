47b07f73b1b6b0824fe57a2479913c88e0a48352
Greg Munn <dev-admin@nxsoftwaretech.com>
2013-11-27 16:29:24 +1100

    View model loader and UI thread scheduler.

---
+++ Core/ViewModel/ViewModelLoader.cs
@@ -0,0 +1,73 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=".cs" company="sgmunn">
+//   (c) sgmunn 2013  
+//
+//   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+//   documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+//   the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+//   to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//   The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+//   the Software.
+//
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+//   THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+//   CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+//   IN THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Mobile.Mvvm.ViewModel
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class ViewModelLoader<TData>
+    {
+        private readonly CancellationTokenSource cancellation;
+
+        private readonly Func<CancellationToken, Task<TData>> loader;
+
+        private readonly Action<TData> updater;
+
+        private readonly TaskScheduler scheduler;
+
+        public ViewModelLoader(Func<CancellationToken, Task<TData>> loader, Action<TData> updater)
+        {
+            this.loader = loader;
+            this.updater = updater;
+            this.cancellation = new CancellationTokenSource();
+        }
+
+        public ViewModelLoader(Func<CancellationToken, Task<TData>> loader, Action<TData> updater, TaskScheduler scheduler)
+        {
+            this.loader = loader;
+            this.updater = updater;
+            this.cancellation = new CancellationTokenSource();
+            this.scheduler = scheduler;
+        }
+
+        public virtual Task Load()
+        {
+            if (this.scheduler != null)
+            {
+                return this.loader(this.cancellation.Token).ContinueWith(this.UpdateViewModel, this.cancellation.Token, TaskContinuationOptions.NotOnCanceled, this.scheduler);
+            }
+
+            return this.loader(this.cancellation.Token).ContinueWith(this.UpdateViewModel, this.cancellation.Token);
+        }
+
+        public virtual void UpdateViewModel(Task<TData> task)
+        {
+            this.updater(task.Result);
+        }
+
+        public virtual void Cancel()
+        {
+            this.cancellation.Cancel();
+        }
+    }
+}
+
